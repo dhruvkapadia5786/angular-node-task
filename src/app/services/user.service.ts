c68941471d69bf269e4fe9a5199f2b1b30c6a165
Blue-Corn <42467285+dhruvkapadia5786@users.noreply.github.com>
2024-03-08 20:55:04 +0530

    Dhruv major chaneges

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../models/user'
 })
 export class UserService {
 
-  private apiUrl = 'https://node-demo-0zx9.onrender.com'
+  private apiUrl = 'https://node-demo-0zx9.onrender.com/api/uses'
 
   constructor(private http: HttpClient) { }
 
